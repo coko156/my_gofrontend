4b025cca12e5712162759231c742f1c842686434
Ray <st.ray156@gmail.com>
2014-06-20 20:03:58 +0800

    Traverse over the IR to get a vector of  functions and the <caller, callee vector> store them in class Escape_analysis

---
+++ escape.cc
@@ -0,0 +1,80 @@
+#include "escape.h"
+
+void 
+Escape_analysis::escape(Gogo* gogo)
+{
+  this.compute_gogo_to_functions(gogo);
+}
+
+Escape_analysis::compute_gogo_to_functions(Gogo* gogo)
+{
+  Call_graph_traverse_functions cgtf(this);
+  gogo->traverse(&cgtf);
+  // now this.function_ is a list of functions.
+}
+
+// used to traverse Gogo to function
+class Call_graph_traverse_functions : public Traverse
+{
+  public:
+    Call_graph_traverse_functions(Escape_analysis* ea) :
+      Traverse(traverse_functions),
+      ea_(ea)
+  { }
+
+  protected:
+      int
+      function(Named_object*);
+
+  private:
+      Escape_analysis* ea_;
+}
+
+int
+Call_graph_traverse_functions::function(Named_object* no)
+{
+  this->ea_->add_function(no);
+
+  go_assert(no->is_function());
+  Func* func = no->func_value();
+
+  Call_graph_traverse_expressions cgte(this->es_, no);
+  func->traverse(&cgte);
+
+  return TRAVERSE_CONTINUE;
+}
+
+// traverse the function
+// This class is used to get the map<caller, callee_vector>
+class Call_graph_traverse_expressions : public Traverse
+{
+  public:
+    // Constructor
+    Call_graph_traverse_expressions(Escape_analysis *ea,
+        Named_object* function) :
+      Traverse(traverse_expressions),
+      ea_(ea), function_(function)
+  { }
+
+  protected:
+    int
+    expression(Expression**);
+
+  private:
+    Escape_analysis* ea_;
+    Named_object* function_;
+}
+
+int
+Call_graph_traverse_expressions::expression(Expression** expr)
+{
+  Call_expression* call_expression = (*expr)->call_expression;
+
+  if (call_expression == NULL)
+    return TRAVERSE_CONTINUE;
+
+  const Named_object* called_function = call_expression->get_function_object;
+
+  if (called_function != NULL) {
+    ea->add_caller_callee();
+}
--- escape.h
+++ escape.h
@@ -4,84 +4,44 @@
 
 #include "gogo.h"
 
+#include <vector>
+#include <map>
 
-// Used for a connection graph
-// A connection graph is for a set of mutually recursive functions.
-// Record a status of the whole set
-class Escape_analysis
+// Firstly get the minimal set of function to run escape analysis on.
+// That is to find strong connected component.
+// Run escapes on total IR.
+//
+// Then build the connection graph for each set of function.
+//
+// Then Walk the graph to give the variables a tag.
+
+class Escape_analysis 
 {
   public:
-    enum Object_type
-    {
+  enum Function_status {
+    // used to mark the status of function been analysised
+    ESCAPE_FUNCTION_UNKNOWN = 0,
+    ESCAPE_FUNCTION_PLANNED,
+    ESCAPE_FUNCTION_STARTED,
+    ESCAPE_FUNCTION_FINISHED
+  };
 
-    };
 
-    // This is for function.
-    enum Escape_level
-    {
-      // Status for function
-      // ESCAPE_FUNC_XXX
-      // need to be clearer
-
-    };
-
-    // escapes -> a list of functions -> vector<vector<Named_object*> >
-    // <vector<vector<int> >::iterator pp;
-    // for (vector<vector<int> > :: iterator :: i1 ;...)
-    //   for (vector<int> :: iterator i2 = i1 -> begin(); ...)
-    //     if (i2.count() == 1) {
-    //     Escape_analysis_func();
-    //     ...
-    //     } else {
-    //     Escape_analysis_func();
-    //     ...
-    //     }
-    // same as escapes
-    // used as traverse
-
-    // Travers the whole IR to get a set of functions.
-    // Use Tarjan to get the connection components.
-    // We build a connection graph for every connection graph.
+    // 总调函数
     static void
-    escapes(Gogo* IR);
+    escapes(Gogo*)  { }
 
-    // Variables needed.
-    // phathom node for escaped node to leak to.
-    // a bool variable to record if it is mutually recursived.
+    // traverse the Gogo* and get a list of functions
+    void
+    compute_gogo_to_functions(Gogo*);
 
-    // Record for depth of the environment
-    int depth;
+  private:
 
-    // A list of functions to be analysised.
-    vector<<vector<Named_object*> > function_list;
+    typedef std::vector<Named_object*> Named_object_vec;
+
+    // map<caller, callee_vector>
+    typedef std::map<const Named_object*, Named_object_vec> Caller_map;
+
+    // used to store functions in a package.
+    Named_object_vec functions_;
 };
-
-
-class Escape_node
-{
-  enum {
-    // ESCAPE_GLOBAL,
-    // ESCAPE_ARG,   // I think it is not needed.
-    // ESCAPE_NONE,
-    // ESCAPE_UNKNOWN,
-    ESCAPE_XXX;
-    // ...
-
-
-
-  }
-  // Record the depth of node in the graph.
-  int loopdepth;
-  
-  // A enum to record the Escape status.
-  // 
-  int Escape_status;
-  
-  // Run Escape_func function to get the edges.
-  // edges(a, b)
-  // a -- reference --> b
-  vector<Escape_node*> edges;
-
-
-}
-
